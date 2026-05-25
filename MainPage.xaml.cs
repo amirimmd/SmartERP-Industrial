e0d4b07cb25656e12475d313c56a1983c7a86dba
amirimmd <m98amiri@gmail.com>
2026-05-25 06:09:55 +0330

    Initial commit: SmartERP Industrial Edition

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,27 +1,20 @@
-﻿using Microsoft.AspNetCore.Components.WebView.Maui;
-
-namespace SmartERP;
+﻿namespace SmartERP;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
-        blazorWebView.BlazorWebViewInitialized += OnBlazorWebViewInitialized;
-    }
-
-    private void OnBlazorWebViewInitialized(object? sender, BlazorWebViewInitializedEventArgs e)
-    {
-#if WINDOWS
-        var coreWebView2 = (e.WebView as Microsoft.UI.Xaml.Controls.WebView2)?.CoreWebView2;
-        if (coreWebView2 != null)
+        blazorWebView.BlazorWebViewInitialized += (_, e) =>
         {
-            // بدون این تنظیم، WebView2 دسترسی Blazor به storage را مسدود می‌کند
-            // و هیچ رویداد @onclick ای به .NET نمی‌رسد
-            coreWebView2.Profile.PreferredTrackingPreventionLevel =
-                Microsoft.Web.WebView2.Core.CoreWebView2TrackingPreventionLevel.None;
-            coreWebView2.Settings.IsLocalContentCanAccessRemoteContent = true;
-        }
+#if WINDOWS
+            var coreWebView2 = (e.WebView as Microsoft.UI.Xaml.Controls.WebView2)?.CoreWebView2;
+            if (coreWebView2 != null)
+            {
+                coreWebView2.Profile.PreferredTrackingPreventionLevel =
+                    Microsoft.Web.WebView2.Core.CoreWebView2TrackingPreventionLevel.None;
+            }
 #endif
+        };
     }
 }
